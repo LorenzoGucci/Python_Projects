b5c32a01dd52dd743f977a75466f740e6f4664e2
Lorenzo Gucci <67734762+LorenzoGucci@users.noreply.github.com>
2020-08-14 09:52:49 +0100

    Update Guess_The_Number.py
    Improved code and added a "guesses" counter

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -1,16 +1,30 @@
+# generate a random number between 1 and 100
+# and asks the user to guess it
+
 import random
-x = random.randint(1,100)
-print("I have picked a number between 1 and 100, can you guess it?")
-ys = input("Guess: ")
-y = int(ys)
-while y != x:
-    if (y < x):
-        print("That's too low, try again!")
-        ys = input("Guess: ")
-        y = int(ys)
-    if (y > x):
-        print("No way, too high, guess again!")
-        ys = input("Guess: ")
-        y = int(ys)
-    if (y == x):
-        print("Yep, that's the number I picked, well done!")
+
+# generate random number
+ran_n = random.randint(1,100)
+
+# initialise guess variable
+guess = ''
+
+# initialise guesses counter
+i= 0
+
+# guessing loop
+while guess != ran_n:
+    # ask user to guess the number
+    guess = int(input('Guess which number I picked:\n'))
+    if guess < ran_n:
+        print('Too low, guess again!')
+    elif guess > ran_n:
+        print('Too high, guess again!')
+    # updates the guesses counter
+    i += 1
+
+# when the user guesses the correct number,
+# exit while loop and print the number and
+# the number of guesses the user has taken
+print("That's right, I picked the number {}!\nYou took {} guesses!"
+    .format(ran_n, i))
